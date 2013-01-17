cf7e85cd63fbe53ef6b261013f561e7d6a1a6fe3
Christoph Leiter <cl@samera.at>
2013-01-17 10:13:29 +0100

    Don't check which mouse button was pressed

--- content.js
+++ content.js
@@ -1,14 +1,12 @@
 document.addEventListener('mouseup', function (e) {
-  if (e.button == 2) {
-    var ws = null;
-    var el = e.srcElement;
+  var ws = null;
+  var el = e.srcElement;
 
-    while (ws == null && el && el.getAttribute) {
-      ws = el.getAttribute("wicketsource");
-      el = el.parentNode;
-    }
-    chrome.extension.sendRequest({
-      wicketsource: ws
-    });
+  while (ws == null && el && el.getAttribute) {
+    ws = el.getAttribute("wicketsource");
+    el = el.parentNode;
   }
+  chrome.extension.sendRequest({
+    wicketsource: ws
+  });
 });
