f6354171b579714099f2fd8d001dbe4a14d92e70
Christoph Leiter <cl@samera.at>
2013-02-14 07:31:04 +0100

    Improve context menu event handling

--- contextMenu.js
+++ contextMenu.js
@@ -1,42 +1,37 @@
 (function() {
-
-  var wicketsource, menu;
-
-  menu = chrome.contextMenus.create({
+  var menu = chrome.contextMenus.create({
     title: "Wicket Source",
-    contexts: ["all"],
-    onclick: function (info) {
-      if (wicketsource) {
-        var ajax = new XMLHttpRequest();
-        var url = "http://localhost:9123/open?src=" + encodeURIComponent(wicketsource);
-        ajax.open("GET", url, true);
-        ajax.send();
-      }
-    },
-    enabled: false
+    contexts: [ "all" ],
+    enabled: false,
+    onclick: function() {}
   });
 
   chrome.extension.onRequest.addListener(function (request) {
     if (request.hasOwnProperty('wicketsource')) {
-      wicketsource = request.wicketsource;
-      if (wicketsource != null) {
-        var idx = wicketsource.indexOf(':');
-        if (idx > -1) {
-          wicketsource = wicketsource.substring(idx + 1);
-        }
-        wicketsource = wicketsource.replace(/\.java:/, ':');
-        chrome.contextMenus.update(menu, {
-          enabled: true,
-          title: "Wicket Source - " + wicketsource
-        });
-      } else {
+      var ws = request.wicketsource;
+      if (ws == null) {
         chrome.contextMenus.update(menu, {
           enabled: false,
           title: "Wicket Source"
         });
+        return;
       }
+      var idx = ws.indexOf(':');
+      if (idx > -1) {
+        ws = ws.substring(idx + 1);
+      }
+      ws = ws.replace(/\.java:/, ':');
+      chrome.contextMenus.update(menu, {
+        enabled: true,
+        title: "Wicket Source - " + ws,
+        onclick: function () {
+          var ajax = new XMLHttpRequest();
+          var url = "http://localhost:9123/open?src=" + encodeURIComponent(request.wicketsource);
+          ajax.open("GET", url, true);
+          ajax.send();
+        }
+      });
     }
   });
-
-
 })();
+
