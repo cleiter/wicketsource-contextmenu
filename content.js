ba1af4425e4cdcaa8a7bf6e9e2436856a30f4eca
Christoph Leiter <cl@samera.at>
2013-02-14 07:01:35 +0100

    Only react on right mouse button

--- content.js
+++ content.js
@@ -2,6 +2,10 @@ document.addEventListener('mousedown', function (e) {
   var ws = null;
   var el = e.target;
 
+  if (e.button != 2) {
+    return;
+  }
+
   while (ws == null && el && el.getAttribute) {
     ws = el.getAttribute("wicketsource");
     el = el.parentNode;
